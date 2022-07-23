363a902635e0f51f386b3a37427338d387c709a9
Rio Arjuna <ri0arjuna@programmer.net>
2022-07-23 13:44:32 +0700

    layouting detail profile

--- app/src/main/java/com/gov/sidesa/ui/profile/detail/DetailProfileActivity.kt
+++ app/src/main/java/com/gov/sidesa/ui/profile/detail/DetailProfileActivity.kt
@@ -2,6 +2,7 @@ package com.gov.sidesa.ui.profile.detail
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.gov.sidesa.R
 
 class DetailProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
