93290ed0d7e715ef631045ef269fd4ae810d2311
Ivon Khalif <ivonkhalif17@gmail.com>
2022-07-24 10:53:44 +0700

    set click to Edit Profile

--- app/src/main/java/com/gov/sidesa/ui/DashboardActivity.kt
+++ app/src/main/java/com/gov/sidesa/ui/DashboardActivity.kt
@@ -1,10 +1,12 @@
 package com.gov.sidesa.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.gov.sidesa.R
 import com.gov.sidesa.base.BaseActivity
 import com.gov.sidesa.databinding.ActivityDashboardBinding
+import com.gov.sidesa.ui.profile.edit.EditProfileActivity
 
 class DashboardActivity : BaseActivity() {
     private lateinit var binding: ActivityDashboardBinding
@@ -13,5 +15,15 @@ class DashboardActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityDashboardBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        mainView()
+    }
+
+    private fun mainView() {
+        with(binding) {
+            buttonAccount.setOnClickListener {
+                startActivity(Intent(this@DashboardActivity, EditProfileActivity::class.java))
+            }
+        }
     }
 }
