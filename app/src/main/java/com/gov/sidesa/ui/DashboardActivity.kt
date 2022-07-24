47c9218a08fd9b73681cb5b959640e159a0d5ef9
Ivon Khalif <ivonkhalif17@gmail.com>
2022-07-24 10:12:14 +0700

    init dashboard

--- app/src/main/java/com/gov/sidesa/ui/DashboardActivity.kt
+++ app/src/main/java/com/gov/sidesa/ui/DashboardActivity.kt
@@ -3,10 +3,15 @@ package com.gov.sidesa.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.gov.sidesa.R
+import com.gov.sidesa.base.BaseActivity
+import com.gov.sidesa.databinding.ActivityDashboardBinding
 
-class DashboardActivity : AppCompatActivity() {
+class DashboardActivity : BaseActivity() {
+    private lateinit var binding: ActivityDashboardBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard)
+        binding = ActivityDashboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
