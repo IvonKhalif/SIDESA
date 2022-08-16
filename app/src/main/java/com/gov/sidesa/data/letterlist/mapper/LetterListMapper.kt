8a8bff1353046618c9cfdf596e5727d6d37519c7
Rio Arjuna <ri0arjuna@programmer.net>
2022-08-16 22:25:55 +0700

    fixing crash on dashboard causes by letterStatus null

--- app/src/main/java/com/gov/sidesa/data/letterlist/mapper/LetterListMapper.kt
+++ app/src/main/java/com/gov/sidesa/data/letterlist/mapper/LetterListMapper.kt
@@ -1,13 +1,8 @@
 package com.gov.sidesa.data.letterlist.mapper
 
-import com.gov.sidesa.data.letter.mapper.asDomain
-import com.gov.sidesa.data.letter.models.LetterTemplateResponse
-import com.gov.sidesa.data.letter.models.WidgetResponse
 import com.gov.sidesa.data.letterlist.response.LetterApprovalResponse
 import com.gov.sidesa.data.letterlist.response.LetterSubmissionResponse
 import com.gov.sidesa.data.letterlist.response.ProfileLetterApprovalResponse
-import com.gov.sidesa.domain.letter.input.models.layout.LetterLayout
-import com.gov.sidesa.domain.letter.input.models.layout.Widget
 import com.gov.sidesa.domain.letter.list.models.LetterApprovalModel
 import com.gov.sidesa.domain.letter.list.models.LetterListApprovalModel
 import com.gov.sidesa.domain.letter.list.models.LetterSubmissionModel
@@ -17,7 +12,7 @@ import com.gov.sidesa.utils.response.RetrofitLetterApprovalResponse
 fun LetterSubmissionResponse.asDomain() = LetterSubmissionModel(
     letterId = letterId,
     letterType = letterType,
-    letterStatus = letterStatus,
+    letterStatus = letterStatus.orEmpty(),
     letterNumber = letterNumber
 )
 
